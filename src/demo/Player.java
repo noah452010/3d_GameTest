c8cacf1ea533bf8f9cf5b9400129d4faf5e2206b
CreepyDutchBoy <83622956+CreepyDutchBoy@users.noreply.github.com>
2026-05-26 00:29:50 -0700

    Update Player.java
    added more clarification (its still ass)

--- src/demo/Player.java
+++ src/demo/Player.java
@@ -2,13 +2,17 @@ package demo;
 
 public class Player {
 		
+
+	// raycasting does not need a 3rd vector for position,
+	// Z vector used here is rotation of player.
 	
 	public static float x_pos;
 	
 	public static float y_pos;
 	
 	public static float y_rot;
-	
+
+	// this makes player information returnable. it's ass
 	
 	public Player(float x, float y, float z) {
 		this.x_pos = x;
