dfd86fc0257d4c3128f36fe212ecfb17ccb6f99b
CreepyDutchBoy <83622956+CreepyDutchBoy@users.noreply.github.com>
2026-05-26 00:33:11 -0700

    Update Map.java
    added more clarification for map

--- src/demo/Map.java
+++ src/demo/Map.java
@@ -2,6 +2,12 @@ package demo;
 
 public class Map {
 	
+
+	// char can stack in as many N dimensions as you provide it, 
+	// with N being how many [] is added. since raycasting is 2D logically,
+	// [][], for char[][] allows for stacking in 2D
+
+	// to reference this, you need to use Map.mesh[X][Y]; from any class.
 	
 	public static char[][] mesh = {
 			{'#', '#', '#', '#', '#', '#', '#', '#'},
