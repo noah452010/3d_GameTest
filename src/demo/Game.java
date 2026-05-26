297f61c00df0ae922fc2f17e907ef0a27d720d77
CreepyDutchBoy <83622956+CreepyDutchBoy@users.noreply.github.com>
2026-05-26 00:26:18 -0700

    Update Game.java
    added more clarification

--- src/demo/Game.java
+++ src/demo/Game.java
@@ -3,7 +3,8 @@ package demo;
 public class Game {
 
 	public static void game() {
-		// TODO Auto-generated method stub
+		// this is where all the player, and game objects are,
+		// instantiated.
 		Player dog = new Player(1.0f, 1.0f, 180f);
 		
 		
