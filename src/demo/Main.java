0699643496380d134c6080fac3c354e61837b985
CreepyDutchBoy <83622956+CreepyDutchBoy@users.noreply.github.com>
2026-05-26 22:27:01 -0700

    Update Main.java
    mistake

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		// start render class 
 		
 		
-		Window window = new Window();
-		Window.r_init();
+		Render render = new Render();
+		Render.r_init();
 
 		
 		// start map class
