70dd60d055e37c093fd7eb536a319de44efe13b4
CreepyDutchBoy <83622956+CreepyDutchBoy@users.noreply.github.com>
2026-05-26 22:28:58 -0700

    Update and rename Render.java to Window.java
    renamed, and switched to GLFW and OpenGL (get it from lwjgl.org/customize)

---
+++ src/demo/Window.java
@@ -0,0 +1,37 @@
+package demo;
+
+import org.lwjgl.opengl.GL11.*;
+import org.lwjgl.glfw.*;
+
+public class Window {
+	public static void r_init(){
+		
+		// this fixes whatever bullshit compat issue under wayland
+		// GLFW has some issue with wayland, so use,
+		// XWayland or X11.
+		
+		GLFW.glfwInitHint(GLFW.GLFW_PLATFORM, GLFW.GLFW_PLATFORM_X11);
+		
+		// GLFW provides it's own init method
+		
+		GLFW.glfwInit();
+		
+		// created long window to create the window
+		
+		long window = GLFW.glfwCreateWindow(800, 600, "window", 0, 0);
+		GLFW.glfwMakeContextCurrent(window);
+		
+		// this keeps GLFW from not closing
+		
+		while (!GLFW.glfwWindowShouldClose(window)) {
+			GLFW.glfwPollEvents();
+			
+		
+		}
+	}
+		
+	public static void render(){
+		
+		}
+	}
+
