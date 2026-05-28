43a1be308a0225ff0120c62cb5ce2d9999326858
noah452010 <noah452010@gmail.com>
2026-05-28 00:12:08 -0700

    fixed inheritance

--- src/GameObject.java
+++ src/GameObject.java
@@ -2,4 +2,10 @@ public class GameObject {
     protected double x;
     protected double y;
     protected double z;
+
+    public GameObject(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
--- src/Rect.java
+++ src/Rect.java
@@ -5,9 +5,7 @@ public class Rect extends GameObject {
     private double pitch;
     private double yaw;
     public Rect(double x, double y, double z, double width, double height, double depth, double pitch, double yaw) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        super(x, y, z);
         this.width = width;
         this.height = height;
         this.depth = depth;
